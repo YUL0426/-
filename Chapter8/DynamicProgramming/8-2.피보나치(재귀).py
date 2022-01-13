c37d1e352e2273ae54c07655835c9ab616ff27c6
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2022-01-13 11:00:50 +0900

    Create 8-2.피보나치(재귀).py

---
+++ Chapter8/DynamicProgramming/8-2.피보나치(재귀).py
@@ -0,0 +1,14 @@
+d = [0] * 100
+
+def fibo(x):
+    # 종료 조건(1 혹은 2일 때 1을 반환)
+    if x == 1 or x == 2:
+        return 1
+    # 이미 계산한 적 있는 문제라면 그대로 반환
+    if d[x] != 0:
+        return d[x]
+    # 아직 계산하지 않은 문제라면 점화식에 따라서 피보나치 결과 변환
+    d[x] = fibo(x - 1) + fibo(x - 2)
+    return d[x]
+
+print(fibo(99))
